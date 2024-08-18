d3f61f520fce0133c1f641d50576f49ef79d6fec
heerokj <jungheerok@gmail.com>
2024-08-19 00:05:44 +0900

    redux count app 만들기

--- redux-app/src/App.jsx
+++ redux-app/src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNumber, removerNumber } from "./redux/modules/counter";
 
 const App = () => {
   const counterReducer = useSelector((state) => state.counter);
-  console.log("counterReducer", counterReducer);
-  return <div>App</div>;
+  const dispatch = useDispatch();
+  const [newNumber, setNewNumber] = useState(0);
+
+  return (
+    <div>
+      {counterReducer.number} <br />
+      <input
+        type="text"
+        onChange={(e) => {
+          return setNewNumber(+e.target.value); //*** e.target.value앞에 + 붙여주면 Number로 형변환됨
+        }}
+        value={newNumber}
+      />
+      <button
+        onClick={() => {
+          dispatch(addNumber(newNumber));
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch(removerNumber(newNumber));
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- redux-app/src/redux/modules/counter.js
+++ redux-app/src/redux/modules/counter.js
@@ -3,9 +3,37 @@ const initialState = {
   number: 0,
 };
 
+// const PULS_ONE = "PULS_ONE"; // value는 상수로 생성 (상수는 보통 대문자)
+// const MINUS_ONE = "MINUS_ONE";
+
+const ADD_NUMBER = "ADD_NUMBER";
+const REMOVE_NUMBER = "REMOVE_NUMBER";
+
+export const addNumber = (payload) => {
+  return {
+    type: ADD_NUMBER,
+    payload: payload,
+  };
+};
+
+export const removerNumber = (payload) => {
+  return {
+    type: REMOVE_NUMBER,
+    payload: payload,
+  };
+};
+
 // 리듀서(함수)만들기
 const counter = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_NUMBER:
+      return {
+        number: state.number + action.payload,
+      };
+    case REMOVE_NUMBER:
+      return {
+        number: state.number - action.payload,
+      };
     default:
       return state;
   }

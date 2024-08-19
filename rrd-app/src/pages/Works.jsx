5c1c3b6a58ca5af716819ccc2c2df93c43c14064
heerokj <jungheerok@gmail.com>
2024-08-19 22:50:27 +0900

    중첩된 라우트(Outlet) 실습

--- rrd-app/src/pages/Works.jsx
+++ rrd-app/src/pages/Works.jsx
@@ -1,20 +1,37 @@
+// src/pages/Works.js
+
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+const data = [
+  { id: 1, todo: "리액트 배우기" },
+  { id: 2, todo: "노드 배우기" },
+  { id: 3, todo: "자바스크립트 배우기" },
+  { id: 4, todo: "파이어 베이스 배우기" },
+  { id: 5, todo: "넥스트 배우기" },
+  { id: 6, todo: "HTTP 프로토콜 배우기" },
+];
 
 const Works = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+  const param = useParams();
+
+  const targetWork = data.find((work) => {
+    return work.id === Number(param.id);
+  });
+
   return (
     <div>
-      <h1>Works 컴포넌트입니다.</h1>
-      <div>현재 페이지 : {location.pathname.slice(1)}</div>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Home으로 이동
-      </button>
+      <h3>현재페이지는 {targetWork.todo} 입니다.</h3>
+      {data.map((work) => {
+        return (
+          <div key={work.id}>
+            <div>할일: {work.id}</div>
+            <Link to={`/works/${work.id}`}>
+              <span style={{ cursor: "pointer" }}>➡️ Go to: {work.todo}</span>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- rrd-app/src/shared/Router.jsx
+++ rrd-app/src/shared/Router.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Works from "../pages/Works";
 import Contact from "../pages/Contact";
+import Works from "../pages/Works";
+import Layout from "./Layout";
+import Dashboard from "./DashBoardLayout";
+import MyPage from "../pages/MyPage";
+import Test from "../pages/Test";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Route */}
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Works" element={<Works />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="works/:id" element={<Works />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route path="mypage" element={<MyPage />} />
+            <Route path="test" element={<Test />} />
+          </Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };

91d2b19056ebaa4275a9e530ac74e8f4ed9cd2bb
heerokj <jungheerok@gmail.com>
2024-08-19 21:27:49 +0900

    useLocation 사용

--- rrd-app/src/pages/Works.jsx
+++ rrd-app/src/pages/Works.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Works = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div>
       <h1>Works 컴포넌트입니다.</h1>
+      <div>현재 페이지 : {location.pathname.slice(1)}</div>
       <button
         onClick={() => {
           navigate("/");

6dd07afda2cd9f86d30a2636e2316093dfa2b35d
heerokj <jungheerok@gmail.com>
2024-08-19 20:52:40 +0900

    useNavigate 사용

--- rrd-app/src/pages/Home.jsx
+++ rrd-app/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Home 컴포넌트입니다.</h1>
+      <button
+        onClick={() => {
+          navigate("/Works");
+        }}
+      >
+        Work로 이동
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- rrd-app/src/pages/Works.jsx
+++ rrd-app/src/pages/Works.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Works = () => {
-  return <div>Works</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Works 컴포넌트입니다.</h1>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home으로 이동
+      </button>
+    </div>
+  );
 };
 
 export default Works;

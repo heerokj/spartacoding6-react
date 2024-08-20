6248010aa5e6789cd918323279d4ec0300a6a63b
heerokj <jungheerok@gmail.com>
2024-08-20 16:31:11 +0900

    useContext

--- hook-app/src/App.jsx
+++ hook-app/src/App.jsx
@@ -1,26 +1,8 @@
-import React from "react";
-import { useEffect } from "react";
-import { useRef } from "react";
+import "./App.css";
+import GrandFather from "./components/GrandFather";
 
-// 홈페이지 로딩 시 아이디에 포커싱을 주고싶다면 => 아이디 인풋을 useRef로 담은다음 포커싱을 주면 됨(즉, 최초렌더링이 될때 포커싱)
-// ref = {idRef} 로 연결!!!
-const App = () => {
-  const idRef = useRef("");
-
-  //최초 렌더링시에만
-  useEffect(() => {
-    idRef.current.focus();
-  }, []);
-  return (
-    <div>
-      <div>
-        아이디 : <input type="text" ref={idRef} />
-      </div>
-      <div>
-        비밀번호 : <input type="password" />
-      </div>
-    </div>
-  );
-};
+function App() {
+  return <GrandFather />;
+}
 
 export default App;

f95d1ca116be17aaef4ffce31f3025d42a89f4d9
heerokj <jungheerok@gmail.com>
2024-08-20 12:50:47 +0900

    clean up 함수

--- hook-app/src/App.jsx
+++ hook-app/src/App.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+// src/SokSae.js
 
-const App = () => {
-  const [value, setValue] = useState("");
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const 속세 = () => {
+  const nav = useNavigate();
+
   useEffect(() => {
-    console.log("useEffect");
-  }, []); // useEffect는 동작에 대한 트리거가 존재 (트리거 : useEffect동작을 이끌어내는 변수)두번째 인자인 의존성배열로 해결 가능
-
-  //의존성 배열 : useEffect가 의존하고 있는 값의 배열
-  //=>이 베열에 값을 넣으면 그 값이 바뀔 때만 useEffect를 실행할게!
-  // => [] 넣으면 리랜더링 될때마다 useEffect 호출되지않아 (트리거 없음)
+    //clean up 함수
+    return () => {
+      console.log(
+        "안녕히 계세요 여러분! 전 이 세상의 모든 굴레와 속박을 벗어 던지고 제 행복을 찾아 떠납니다! 여러분도 행복하세요~~!"
+      );
+    };
+  }, []);
 
   return (
-    <div>
-      <h1>useEffect</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-    </div>
+    <button
+      onClick={() => {
+        nav("/todos");
+      }}
+    >
+      속세를 벗어나는 버튼
+    </button>
   );
 };
 
-export default App;
+export default 속세;

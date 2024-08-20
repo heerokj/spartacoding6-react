6508092ab9005006ebda3e6edc95b8719a9cd812
heerokj <jungheerok@gmail.com>
2024-08-20 12:46:20 +0900

    useEffect

--- hook-app/src/App.jsx
+++ hook-app/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import Child from "./components/Child";
+import { useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    console.log("useEffect");
+  }, []); // useEffect는 동작에 대한 트리거가 존재 (트리거 : useEffect동작을 이끌어내는 변수)두번째 인자인 의존성배열로 해결 가능
+
+  //의존성 배열 : useEffect가 의존하고 있는 값의 배열
+  //=>이 베열에 값을 넣으면 그 값이 바뀔 때만 useEffect를 실행할게!
+  // => [] 넣으면 리랜더링 될때마다 useEffect 호출되지않아 (트리거 없음)
 
   return (
     <div>
-      <h1>부모</h1>
-      <span>{count}</span>
-      <Child count={count} setCount={setCount}></Child>
+      <h1>useEffect</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
     </div>
   );
 };
